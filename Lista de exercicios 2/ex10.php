ae69de9cf14fb48c5e731a82f0af8c53a5ab64f3
rafaellealdantas <119167570+rafaellealdantas@users.noreply.github.com>
2024-04-12 10:30:11 -0300

    Add files via upload

---
+++ Lista de exercicios 2/ex10.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pergunta em que estuda. Pede para digitar M-matutino V-Vespertino N- Noturno. Imprima a mensagem "Bom Dia!", "BoaTarde!" "Boa Noite!" "Valor Inválido!".</title>
+</head>
+<body>
+    <form action="" method="get">
+        <p>
+            <label for="turno">digite o turno que você estuda (M-matutino V-Vespertino N- Noturno): </label>
+            <input type="text" id="turno" name="turno">
+        </p>
+        
+    </form>
+</body>
+</html>
